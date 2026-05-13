5f038b5eacc8457d4ce3bd7ec11d78ddda1676ec
bogeyturn <user@example.com>
2026-05-13 16:40:09 +0200

    disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
         },
         plugins: [wasm(), topLevelAwait()],
     },
-    ssr: true,
+    ssr: false,
     nitro: {
         preset: "vercel", // or 'vercel', 'cloudflare', etc.
     },
